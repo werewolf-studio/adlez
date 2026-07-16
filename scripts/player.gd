d5e3a8a79e7f8d6c1746739bcc2f38e7342a7dc8
krystoham <krystof.ham@volny.cz>
2026-07-16 20:16:24 +0200

    Add comment for readeble code

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,13 +3,11 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-
+## gets direction and moves
 func _physics_process(delta: float) -> void:
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")	
 	if direction:
 		velocity = direction * SPEED
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
-
 	move_and_slide()
